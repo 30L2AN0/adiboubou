97f1d99fdab16b05d326618f1dd698e0c2bc0504
Thittho <bouchetthomas@orange.fr>
2021-12-16 01:11:01 +0100

    Bugs fixed, maps up to date

--- adiboubou.py
+++ adiboubou.py
@@ -5,63 +5,64 @@
 
 from __init__ import *
 
-global liste_obstacles, liste_blocs, liste_regles, taille, grille, liste_liste_objets, images_blocs, images_obstacles
+global liste_obstacles, liste_blocs, liste_regles, taille, grille, liste_liste_objets, images_blocs, images_obstacles, images_autres
 
 from regles import *
 exec(open("regles.py").read())
 from rewind import *
 exec(open("rewind.py").read())
-from graphismes import *
 exec(open("graphismes.py").read())
 
 #on met à jour les listes en fonction de la map choisie
-from map2 import *
-exec(open("map2.py").read())
+from map1 import *
+exec(open("map1.py").read())
 
 liste_liste_objets = [(copy_blocs(liste_blocs), copy_obstacles(liste_obstacles))]
 grille, images_blocs, images_obstacles = afficher_grille0(liste_blocs, liste_obstacles)
 liste_regles, images_obstacles = nouvelles_regles(liste_blocs, images_obstacles)
+images_autres = []
 for i in range(len(liste_obstacles)):
-    images_obstacles = nouvelle_grille_is(liste_obstacles[i], i, images_obstacles)
+    images_obstacles = nouvelle_grille_is(liste_regles, liste_obstacles[i], i, images_obstacles)
 
 
 def gauche(event):
-    global liste_regles, grille, liste_obstacles, liste_blocs, liste_regles, liste_liste_objets, images_blocs, images_obstacles
-    if checker_regles(liste_regles):
+    global liste_regles, grille, liste_obstacles, liste_blocs, liste_regles, liste_liste_objets, images_blocs, images_obstacles, images_autres
+    if checker_regles(liste_regles, images_autres):
         a = check_deplacement([-1,0], liste_regles, liste_blocs, liste_obstacles, grille, liste_liste_objets)
-        grille, images_blocs, images_obstacles = afficher_grille(liste_blocs, liste_obstacles, images_blocs, images_obstacles, liste_liste_objets, a)
         liste_regles, images_obstacles = nouvelles_regles(liste_blocs, images_obstacles)
-    checker_regles(liste_regles)
+        grille, images_blocs, images_obstacles = afficher_grille(liste_blocs, liste_obstacles, images_blocs, images_obstacles, liste_liste_objets, a)
+    checker_regles(liste_regles, images_autres)
 
 def droite(event):
-    global liste_regles, grille, liste_obstacles, liste_blocs, liste_regles, liste_liste_objets, images_blocs, images_obstacles
-    if checker_regles(liste_regles):
+    global liste_regles, grille, liste_obstacles, liste_blocs, liste_regles, liste_liste_objets, images_blocs, images_obstacles, images_autres
+    if checker_regles(liste_regles, images_autres):
         a = check_deplacement([1,0], liste_regles, liste_blocs, liste_obstacles, grille, liste_liste_objets)
-        grille,images_blocs, images_obstacles = afficher_grille(liste_blocs, liste_obstacles, images_blocs, images_obstacles, liste_liste_objets, a)
         liste_regles, images_obstacles = nouvelles_regles(liste_blocs, images_obstacles)
-    checker_regles(liste_regles)
+        grille,images_blocs, images_obstacles = afficher_grille(liste_blocs, liste_obstacles, images_blocs, images_obstacles, liste_liste_objets, a)
+    checker_regles(liste_regles, images_autres)
 
 def haut(event):
-    global liste_regles, grille, liste_obstacles, liste_blocs, liste_regles, liste_liste_objets, images_blocs, images_obstacles
-    if checker_regles(liste_regles):
+    global liste_regles, grille, liste_obstacles, liste_blocs, liste_regles, liste_liste_objets, images_blocs, images_obstacles, images_autres
+    if checker_regles(liste_regles, images_autres):
         a = check_deplacement([0,-1], liste_regles, liste_blocs, liste_obstacles, grille, liste_liste_objets)
-        grille, images_blocs, images_obstacles = afficher_grille(liste_blocs, liste_obstacles, images_blocs, images_obstacles, liste_liste_objets, a)
         liste_regles, images_obstacles = nouvelles_regles(liste_blocs, images_obstacles)
-    checker_regles(liste_regles)
+        grille, images_blocs, images_obstacles = afficher_grille(liste_blocs, liste_obstacles, images_blocs, images_obstacles, liste_liste_objets, a)
+    checker_regles(liste_regles, images_autres)
 
 def bas(event):
-    global liste_regles, grille, liste_obstacles, liste_blocs, liste_regles, liste_liste_objets, images_blocs, images_obstacles
-    if checker_regles(liste_regles):
+    global liste_regles, grille, liste_obstacles, liste_blocs, liste_regles, liste_liste_objets, images_blocs, images_obstacles, images_autres
+    if checker_regles(liste_regles, images_autres):
         a = check_deplacement([0,1], liste_regles, liste_blocs, liste_obstacles, grille, liste_liste_objets)
-        grille, images_blocs, images_obstacles = afficher_grille(liste_blocs, liste_obstacles, images_blocs, images_obstacles, liste_liste_objets, a)
         liste_regles, images_obstacles = nouvelles_regles(liste_blocs, images_obstacles)
-    checker_regles(liste_regles)
+        grille, images_blocs, images_obstacles = afficher_grille(liste_blocs, liste_obstacles, images_blocs, images_obstacles, liste_liste_objets, a)
+    checker_regles(liste_regles, images_autres)
 
 def revenir_temps(event):
-    global liste_regles, liste_blocs, liste_obstacles, liste_liste_objets, grille, images_blocs, images_obstacles
-    grille, images_blocs, images_obstacles = afficher_grille_rewind(liste_regles, liste_blocs, liste_obstacles, images_blocs, images_obstacles, liste_liste_objets)
+    global liste_regles, liste_blocs, liste_obstacles, liste_liste_objets, grille, images_blocs, images_obstacles, images_autres
+    grille, images_blocs, images_obstacles = afficher_grille_rewind(liste_blocs, liste_obstacles, images_blocs, images_obstacles, liste_liste_objets)
     liste_blocs, liste_obstacles = rewind(liste_liste_objets, liste_blocs, liste_obstacles)
     liste_regles, images_obstacles = nouvelles_regles(liste_blocs, images_obstacles)
+    checker_regles(liste_regles, images_autres)
 
 top.bind("<Left>", gauche)
 top.bind("<Right>", droite)
